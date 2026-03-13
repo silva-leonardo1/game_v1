e706a2b97231c62234edf79545817a354d8909bd
silva-leonardo1 <leonardo403silva@gmail.com>
2026-03-13 13:45:58 -0300

    adsa

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+    //Outline outline;
+    public string message;
+    public UnityEvent onInteraction;
+    void Start()
+    {
+        //outline = GetComponent<Outline>();
+        //outline.enabled = false;
+    }
+
+    public void DisableOutline()
+    {
+        //outline.enabled = false;
+    }
+}
